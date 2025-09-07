06902bc765740a3a8b48dc9c8c372a4031ae8356
bogdan22tsb <bogdan22tsb@gmail.com>
2025-09-07 13:50:11 +0300

    add scss to the project & add some boilerplate code to work with

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,59 @@
 import "./categories.styles.scss";
 
+const Cnt = ({ title, imageUrl }) => {
+    return (
+        <>
+            <div className="category-container">
+                <div className="background-image" style={{
+                    backgroundImage: `url(${imageUrl})`
+                }}/>
+                <div className="category-body-container">
+                    <h2>{title}</h2>
+                    <p>Shop now!</p>
+                </div>
+            </div>
+        </>
+    );
+};
+
 const App = () => {
-    return <></>;
+    const categories = [
+        {
+            id: 1,
+            title: "hats",
+            imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
+        },
+        {
+            id: 2,
+            title: "jackets",
+            imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
+        },
+        {
+            id: 3,
+            title: "sneakers",
+            imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
+        },
+        {
+            id: 4,
+            title: "womens",
+            imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
+        },
+        {
+            id: 5,
+            title: "mens",
+            imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
+        },
+    ];
+
+    return (
+        <>
+            <div className="categories-container">
+                {categories.map(({ id, title, imageUrl }) => (
+                    <Cnt key={id} title={title} imageUrl={imageUrl}/>
+                ))}
+            </div>
+        </>
+    );
 };
 
 export default App;
